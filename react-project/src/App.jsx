3626b4aa612289c9ef41c5008e76b56411a3816e
Douglas Titze <douglasmarcost2@gmail.com>
2024-11-14 19:23:07 -0500

    Create new components

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+let langauge = "Javascript"
 
+function Header(){
+  return (
+    <header>
+      <h1>!Doug's Famous Website!</h1>
+    </header>
+  )
+}
+
+const activities = [
+  "Hiking",
+  "Working out",
+  "Pickle ball"
+]
+
+const activityObjects = activities.map((food,i) => ({
+  id: i,
+  title: food
+}))
+
+function Main({activities,today}){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Welcome to the website</h2>
+    </div>
+    <main>
+      <img src="https://github.com/douglastitze.png" height = {400} alt='A handsome man smiling wearing a suit and glasses with a red tie'></img>
+      <p>These are some things I like to do:</p>
+      <ul>
+        {activities.map((food => (
+          <li key = {food.id}>{food.title}</li>
+          )))
+        }
+      </ul>
+      <p><small>{today}</small></p>
+    </main>
+  </>
+  )
+}
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+      <Main activities={activityObjects} today={new Date().getFullYear()}/>
+    </div>
   )
 }
 
