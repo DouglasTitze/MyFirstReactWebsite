1f76b2759c982db212ed5a92624f86f00e23cf2b
Douglas Titze <douglasmarcost2@gmail.com>
2024-11-14 20:43:18 -0500

    Add fundamentals of jsx syntax and react hooks

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import { useState, useReducer, useEffect } from 'react'
 
-let langauge = "Javascript"
-
-function Header(){
+function Header({openStatus, changeStatus}){
   return (
     <header>
       <h1>!Doug's Famous Website!</h1>
+      <button onClick={changeStatus}>{openStatus ? "Close" : "Open"} The Page?</button>
     </header>
   )
 }
@@ -43,11 +43,26 @@ function Main({activities,today}){
 }
 
 function App() {
+  // Avoid declaring status in components to avoid sync issues, instead pass the status as properties
+  // const [status, setStatus] = useState(true);
+  // Using reducer instead of useState we can inject what the toggle is supposed to do prior to passing it as a property
+  const [status, toggle] = useReducer((status) => !status, true)
 
+  // Runs anytime the status variable changes
+  useEffect(() => {console.log(`The page status is ${status ? "open" : "closed"}.`)})
+  // Am empty depenency array means it only logs this message on the first render
+  // useEffect(() => {console.log(`The page status is ${status ? "open" : "closed"}.`)}, [])
+  // To be more specific we can add 'status' which again makes it render everytime status updates
+  // useEffect(() => {console.log(`The page status is ${status ? "open" : "closed"}.`)}, [status])
+
+  // Move this button to header to show how to change state from a component
+  // <button onClick={() => setStatus(!status)}>{status ? "Close" : "Open"} The Page?</button>
   return (
     <div>
-      <Header/>
-      <Main activities={activityObjects} today={new Date().getFullYear()}/>
+      <h1>This page is currently {status ? "open" : "closed"}</h1>
+      
+      <Header openStatus = {status} changeStatus = {toggle}/>
+      <Main activities={activityObjects} today={new Date().getFullYear()} openStatus = {status}/>
     </div>
   )
 }
